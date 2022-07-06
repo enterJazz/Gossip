3b3ad2ef81586eb5607e7f672110d0c9fa324c85
Wlad Meixner <hi@iamwlad.com>
2022-07-06 11:22:29 +0200

    feat: update logger format

--- gossip/src/main.rs
+++ gossip/src/main.rs
@@ -1,6 +1,10 @@
-use std::path::PathBuf;
+mod communication;
+use chrono::Local;
 use clap::Parser;
-use env_logger;
+use env_logger::Builder;
+use log::LevelFilter;
+use std::io::Write;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -11,7 +15,19 @@ struct Args {
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+
     let args = Args::parse();
     println!("Hello, world!");
 }
