ea6f8a0c011f97ff63d62a9a9c7322a1a3dda9eb
Robert Schambach <robert.schambach@tum.de>
2022-09-08 16:04:21 +0200

    make progress on api and knowledge base integr.

--- gossip/src/broadcaster/mod.rs
+++ gossip/src/broadcaster/mod.rs
@@ -5,17 +5,21 @@ use log::{debug, error, info, log_enabled, warn, Level};
 use std::{net::SocketAddr, sync::Arc};
 use thiserror::Error;
 
-use tokio::sync::{mpsc, Mutex};
+use tokio::{sync::{mpsc, Mutex}, net::TcpListener, io};
 
 use crate::{
     communication::{api, p2p},
-    config::Config,
+    config::Config, publisher,
 };
 
 #[derive(Error, Debug)]
 pub enum BroadcasterError {
     #[error("p2p server failed: {0}")]
     P2PServerFailed(#[from] p2p::server::ServerError),
+    #[error("api server failed: {0}")]
+    APIServerFailed(#[from] api::server::Error),
+    #[error("io error: {0}")]
+    IOError(#[from] io::Error),
 }
 
 struct View {
@@ -68,13 +72,32 @@ impl Broadcaster {
         todo!("start api server");
         todo!("start p2p server");
 
+        // broadcaster - api channels
         let (broadcaster_api_tx, mut broadcaster_api_rx) =
             mpsc::channel::<api::message::ApiMessage>(512);
         let (api_broadcaster_tx, api_broadcaster_rx) =
-            mpsc::channel::<Result<api::message::ApiMessage, api::message::Error>>(512);
+            mpsc::channel::<Result<api::message::ApiMessage, api::server::Error>>(512);
+        // broadcaster - p2p channels
         let (broadcaster_p2p_tx, mut broadcaster_p2p_rx) = mpsc::channel::<p2p::message::Data>(512);
         let (p2p_broadcaster_tx, p2p_broadcaster_rx) =
             mpsc::channel::<(p2p::message::Data, SocketAddr)>(512);
+        // api - publisher channels
+        let (pub_api_tx, pub_api_rx) = mpsc::channel(512);
+        let (api_pub_tx, api_pub_rx) = mpsc::channel(512);
+        
+        let publisher = publisher::Publisher::new(pub_api_tx, api_pub_rx).await;
+        let api_listener = TcpListener::bind(self.config.get_api_address()).await?;
+        let rps_address = todo!("get the rps addr from config");
+        tokio::spawn(async move {
+            api::server::run(
+                api_listener,
+                pub_api_rx,
+                api_pub_tx,
+                broadcaster_api_rx,
+                api_broadcaster_tx,
+            rps_address).await;
+        });
+
 
         let p2p_server = p2p::server::run(
             self.config.get_p2p_address(),
@@ -98,13 +121,18 @@ impl Broadcaster {
                                         knowledge_base.update_sent_item_to_peers(data, reached_peers)
                                             .await
                                             .unwrap_or_else(|e| error!("failed to push knowledge item: {}", e));
-
                                     },
                                     Some(Ok(api::message::ApiMessage::RPSPeer(peer))) => {
                                         // TODO: add host key parameter
                                         // TODO: parse PortMapRecord to get correct port for P2P peer
                                         // try connecting to new peer
-                                        p2p_server.connect(peer.address).await;
+                                        // TODO: @wlad do we need any extra handling here if rps peer connect fails
+                                        if let Err(e) = p2p_server.connect(peer.address).await {
+                                            error!("failed to connect to RPS-supplied peer: {}", e);
+                                            continue
+                                        };
+
+                                        // TODO: get peer identity after connecting to new RPS-supplied peer s.t. we can dump knowledge base items into new peer
                                     },
                                     Some(Ok(_)) => error!("received unexpected message from API in broadcaster"),
                                     Some(Err(err)) => {
