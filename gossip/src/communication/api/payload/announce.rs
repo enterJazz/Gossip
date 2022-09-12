abc2c5b99144fe5d94681c8d47797d2ce79a64d8
Robert Schambach <robert.schambach@tum.de>
2022-09-12 20:26:26 +0200

    fix: announce parsing; size calculated incorrectly

--- gossip/src/communication/api/payload/announce.rs
+++ gossip/src/communication/api/payload/announce.rs
@@ -2,6 +2,9 @@ use crate::communication::api::parse::{get_size, get_u16, get_u8, skip, Error};
 use bytes::Bytes;
 use std::io::Cursor;
 
+/// size of fixed length fields of announce message
+const ANNOUNCE_PREAMBLE_SIZE: u16 = 4;
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Announce {
     pub ttl: u8,
@@ -15,7 +18,8 @@ impl Announce {
         // reserved
         skip(src, 1)?;
         let data_type = get_u16(src)?;
-        let data = get_size(src, size as usize)?;
+        let data_size = size - ANNOUNCE_PREAMBLE_SIZE;
+        let data = get_size(src, data_size as usize)?;
         Ok(Announce {
             ttl,
             data_type,
